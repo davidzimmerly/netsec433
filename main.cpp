43aeddb4101fbde58375322ccf575c61d205075e
davidzimmerly <daz@uoregon.edu>
2017-01-23 16:57:36 -0800

    Added class structure back with pass by reference

--- main.cpp
+++ main.cpp
@@ -1,43 +1,76 @@
 #include <string>
 #include <iostream>
 
-void anyKey();
+bool anyKey();
 
-std::string key = "94307803947898";
+class encrypt_ram{
+    private:
+        std::string key = "94307803947898";
+    public:
+        void encrypt(std::string &message){
+            for (int x = 0; x < message.size(); x++) { message[x] ^= key[x]; }//simple XOR encrypt with key (key should be as long as input, ensure key size on update)
+        }
+        void decrypt(std::string &message){
+           encrypt(message);
+        }
+
+};
 
 int main()
 {
+    bool done = false;
+    encrypt_ram* er = new encrypt_ram();
+    while(!done){
 
         std::string message =  "Sammy"; //value is exposed in memory
-
-        for (int x = 0; x < message.size(); x++) { message[x] ^= key[x]; }//simple XOR encrypt with key (key should be as long as input, ensure key size on update)
-        }
+        er->encrypt(message);
 
         //value is encrypted in memory
 
         std::cout << "Encrypted: " << message<< std::endl;
-        anyKey();
+        done = anyKey();
+
+        if (!done){
+            er->decrypt(message);
+
+            //value is exposed in memory
+
+            std::cout << "Decrypted: " << message<< std::endl;
+            done = anyKey();
+        }
+
+        //need to re-decrypt to access again, needs to be cleared to get out of memory
+        if (!done){
+            std::cout<< "re encrypt decrypted value"<< std::endl;
+            er->encrypt(message);
+            done = anyKey();
+        }
+    }
 
 
-        for (int x = 0; x < message.size(); x++) { message[x] ^= key[x]; }//simple XOR encrypt with key (key should be as long as input, ensure key size on update)
 
-        std::cout << "Decrypted: " << message<< std::endl;//value is exposed in memory
-        anyKey();
-
-        std::cout<< "cleared decrypted value"; //need to re-decrypt to access again, needs to be cleared to get out of memory
-        message = "";
-        anyKey();
-
-	}
+    delete er;
 	return 0;
 }
 
 
-void anyKey()
+//we probably want some functionality to clear all variable values on exit.
+
+bool anyKey()
 {
-    std::cout << "Press any key + enter to continue..." << std::endl;
+    std::cout << "Press any key + enter to continue...x/exit to exit" << std::endl;
     std::string c;
     std::cin >> c;
-    std::getline(std::cin, c);
+
+    if (c=="x" || c=="exit"){
+        std::getline(std::cin, c);
+        return true;
+    }
+    else{
+        std::getline(std::cin, c);
+        return false;
+
+    }
+
 }
 

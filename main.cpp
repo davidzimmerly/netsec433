008a340e97900b35e7af7ffa3fcafcf921e0a379
davidzimmerly <daz@uoregon.edu>
2017-01-27 21:09:51 -0800

    Added 1st permutation method for DES, and some binary display methods

--- main.cpp
+++ main.cpp
@@ -17,9 +17,14 @@ void toggleBit(unsigned short &input, int bit){ input ^= 1 << bit ; }
 bool checkBit(unsigned long long &input, int bit){ return (input>>bit)&1;}
 bool checkBit(unsigned int &input, int bit){ return (input>>bit)&1;}
 bool checkBit(unsigned short &input, int bit){ return (input>>bit)&1;}
-void changeBit(unsigned long long &input, int bit, bool value){ input ^= (-value ^input) & (1 << bit);}
-void changeBit(unsigned int &input, int bit, bool value){ input ^= (-value ^input) & (1 << bit);}
-void changeBit(unsigned short &input, int bit, bool value){ input ^= (-value ^input) & (1 << bit);}
+void changeBit(unsigned long long &input, unsigned long long bit, bool value){ unsigned long long one = 1; input ^= (-value ^input) & (one << bit);}
+/*void changeBit(unsigned int &input, int bit, bool value){ input ^= (-value ^input) & (1 << bit);}
+void changeBit(unsigned short &input, int bit, bool value){ input ^= (-value ^input) & (1 << bit);}*/
+unsigned long long* permuteKey(unsigned long long &input);
+void displayBinary(unsigned long long input);
+void displayBinary(unsigned int input);
+void displayBinary(unsigned short input);
+
 
 class encrypt_ram{
     private:
@@ -70,8 +75,17 @@ int main()
     }
     //unsigned short ld, rd;
     */
-
+    unsigned long long des_key = 1383827165325090801;
+    unsigned long long* permutation1 = new unsigned long long;
+    permutation1 = permuteKey(des_key);
+    //setBit()
+    displayBinary(*permutation1);
+    std::cout << std::endl;
+    std::cout << *permutation1 << std::endl;
+    //changeBit(*permutation1, 2, 1);
+    //std::cout << *permutation1 << std::endl;
     delete er;
+    delete permutation1;
 	return 0;
 }
 
@@ -93,6 +107,41 @@ bool anyKey()
         return false;
     }
 }
+
+const int pc_1 [56] = { 57,   49,    41,   33,    25,    17,    9,
+                        1,   58,    50,   42,    34,    26,   18,
+                        10,    2,    59,   51,    43,    35,   27,
+                        19,   11,     3,   60,    52,    44,   36,
+                        63,   55,    47,   39,    31,    23,   15,
+                        7,   62,    54 ,  46,    38,    30,   22,
+                        14,    6,    61,   53,    45,    37,   29,
+                        21,   13,     5,   28,    20,    12,    4 };
+
+unsigned long long* permuteKey(unsigned long long &input){
+    unsigned long long* permutation= new unsigned long long;
+    *permutation = 0;
+    for (int x=0; x<56; x++){//set 56 bit key, the upper bits should be blank
+        changeBit(*permutation,55-x,checkBit(input,63-pc_1[x]+1));
+    }
+    return permutation;
+}
+
+void displayBinary(unsigned long long input){
+    for (int x=63; x>=0; x--)
+        std::cout << checkBit(input,x);
+    std::cout << std::endl;
+}
+void displayBinary(unsigned int input){
+    for (int x=31; x>=0; x--)
+        std::cout << checkBit(input,x);
+    std::cout << std::endl;
+}
+void displayBinary(unsigned short input){
+    for (int x=15; x>=0; x--)
+        std::cout << checkBit(input,x);
+    std::cout << std::endl;
+}
+
 
 void bitwiseSplit(unsigned long long input, unsigned long &leftDigits, unsigned long &rightDigits){
     //64 bit  => 32 x 2 version

2d643491a492e1d044f66ca1a8d75a268ad8b450
jsbrown86 <jbrown14@uoregon.edu>
2017-03-15 16:14:35 -0700

    Fixed inf. value rolling, rm'd BigInteger dependency...

--- encrypt_ram_rsa.cpp
+++ encrypt_ram_rsa.cpp
@@ -1,9 +1,6 @@
-// RSA Encryption Example by Jacob Brown
-// Utilizes BigInteger to store very large
-// numbers and manipulate them. Very slow
-// at the moment.
 #include "encrypt_ram_rsa.h"
 
+// Determines if two values are coprimes by GCD
 int encrypt_ram_rsa::gcd(long int x, int y) {
 	while (y) {
 		int temp = x;
@@ -13,6 +10,7 @@ int encrypt_ram_rsa::gcd(long int x, int y) {
 	return x;
 }
 
+// Returns totient's coprime
 long int encrypt_ram_rsa::coprime(long int totient) {
 	long int b = (rand() % totient) + 1;
 
@@ -23,34 +21,26 @@ long int encrypt_ram_rsa::coprime(long int totient) {
 	return b;
 }
 
+// Determines modular multiplicative inverse
 int encrypt_ram_rsa::modInverse(int a, int m) {
 	a = a % m;
 	for (int x = 1; x < m; x++) {
-		if ((a*x) % m == 1) {
+		if ((a * x) % m == 1) {
 			return x;
 		}
 	}
 	return 0;
 }
 
-int expo(int a, int b){
-  int result = 1;
-
-  while (b){
-    if (b%2==1){
-      result *= a;
-    }
-    b /= 2;
-    a *= a;
-  }
-
-  return result;
-}
-
-void encrypt_ram_rsa::generateValues(){
-	while(d == 0){
-		p = rand_primes[rand() % 168];
-		q = rand_primes[rand() % 168];
+// Generates values used in encryption/decryption.
+// If there isn't a modular multiplicative inverse
+// for value e, it rolls again. Primes below 1000
+// are used.
+void encrypt_ram_rsa::generateValues(){	
+	d = 0;
+	while (d == 0){
+		p = rand_primes[(rand() % 167)];
+		q = rand_primes[(rand() % 167)];
 		n = p * q;
 		totient = (p - 1) * (q - 1);
 		e = coprime(totient);
@@ -58,104 +48,58 @@ void encrypt_ram_rsa::generateValues(){
 	}
 }
 
+void encrypt_ram_rsa::eraseStrings(){
+	for (uint i = 0; i < strin.size(); i++){
+		strin.at(i) = 'x';
+		strcp.at(i) = 'x';
+	}
+}
+
+// Encrypts string character-by-character and places
+// it in vector fn
 void encrypt_ram_rsa::encryptString(){
-	vector<long long int> k;
-	std::string x;
-	int y;
-	BigInteger z;
+	long long int z, y;
+	
+	std::cerr << "\nEncrypted string: ";
 	
 	// Encrypts string
-	for (uint i = 0; i < name.size(); i++) {
-		x = to_string((int)name.at(i));
-		y = (int)name.at(i);
-		z = BigInteger(x);
-		name.at(i) = 'x';
+	for (uint i = 0; i < strin.size(); i++) {
+		z = static_cast<int>(strin.at(i));
+		y = z;
 		for (int j = 1; j < e; j++) {
 			z *= y;
 			z %= n;
 		}
-		fn.push_back(stoll(z.getNumber()));
+		fn.push_back(z);
+		std::cerr << fn.at(i) << " ";
 	}
-	std::cerr << z.getNumber() << std::endl;
+	std::cerr << std::endl;
 }
 
-void encrypt_ram_rsa::decryptString(){
-	std::cerr << "Yo!" << std::endl;
-	BigInteger z2;
-	long long int y2;
-	string s;
+// Decrypts string character-by-character and displays
+// string if values were usable
+int encrypt_ram_rsa::decryptString(){
+	long long int z2, y2;
+	strcp = "";
 
-	// Decrypts string
-	for (uint i = 0; i < name.size(); i++) {
-		z2 = BigInteger(fn.at(i));
-		y2 = fn.at(i);
+	for (uint i = 0; i < fn.size(); i++) {
+		z2 = fn.at(i);
+		y2 = z2;
 		for (int j = 1; j < d; j++) {
 			z2 *= y2;
 			z2 %= n;
 		}
-		int guy = stoi(z2.getNumber());
-		s.push_back(static_cast<char>(guy));
-	}
-	std::cerr << s << std::endl;
-}
-
-void encrypt_ram_rsa::encryptPIN(){
-	BigInteger pin_temp = BigInteger(pin);
-
-	/*
-	// Encrypts int
-	for (int i = 1; i < e; i++) {
-		pin_temp *= pin;
-	}
-
-	encrypted_pin = BigInteger(pin_temp % n);
-	*/
-	
-	long long int result = 1;
-	long long int exponent = e;
-	
-	while (exponent > 0){
-		if (exponent % 2 == 1){
-			result = (result * pin) % n;
-		}
-		exponent = exponent >> 1;
-		pin = (pin * pin) % n;
+		strcp.push_back(static_cast<char>(z2));
 	}
 	
-	encrypted_pin = result;
-	enc_p = result;
+	// Clears vector in case bad values were used
+	fn.clear();
 	
-	//std::cerr << "Public Key PIN: " << pin_temp.getNumber() << std::endl;
-	//std::cerr << "Encrypted PIN: " << encrypted_pin.getNumber() << std::endl;
-	std::cerr << "Encrypted PIN: " << result << std::endl;
-}
-
-void encrypt_ram_rsa::decryptPIN(){
-	decrypted_pin = BigInteger(encrypted_pin);
-	//dec_mult_pin = BigInteger(encrypted_pin);
-	
-	/*
-	// Decrypts Int
-	for (int i = 1; i < d; i++) {
-		decrypted_pin *= dec_mult_pin;
+	if (strin == strcp){
+		std::cerr << strcp << " at " << &strcp << std::endl;
+		return 1;
+	} else {
+		std::cerr << "Bad values - trying again..." << std::endl;
+		return -1;
 	}
-	
-	decrypted_pin %= n;
-	*/
-	
-	long long int result = 1;
-	long long int exponent = d;
-	long long int dec_mult = enc_p;
-	
-	while (exponent > 0){
-		if (exponent % 2 == 1){
-			result = (result * dec_mult) % n;
-		}
-		exponent = exponent >> 1;
-		dec_mult = (dec_mult * dec_mult) % n;
-	}
-	
-	decrypted_pin = result;
-
-	std::cerr << "Decrypted PIN: " << result << std::endl;
 }

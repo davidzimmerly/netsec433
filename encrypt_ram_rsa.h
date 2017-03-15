7a13ef37f709df0157cab7c8c5347b68a68dad74
jsbrown86 <jbrown14@uoregon.edu>
2017-03-15 16:13:23 -0700

    Removed BigInteger dependency among other things

--- encrypt_ram_rsa.h
+++ encrypt_ram_rsa.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <sstream>
 #include <cmath>
 #include <stdio.h>
@@ -7,7 +8,6 @@
 #include <iomanip>
 #include <math.h>
 #include <vector>
-#include "BigInteger.h"
 
 class encrypt_ram_rsa{
 	public:
@@ -16,22 +16,15 @@ class encrypt_ram_rsa{
 		long int coprime(long int totient);
 		int modInverse(int a, int m);
 		void generateValues();
+		void eraseStrings();
 		void encryptString();
-		void decryptString();
-		void encryptPIN();
-		void decryptPIN();
+		int decryptString();
 		
-		long long int p, q, n, totient, e;
-		long long int enc_p = 0;
-		long long int d = 0;
-		vector<long long int> fn;
+		long long int p, q, n, totient, e, d;
+		std::vector<long long int> fn;
+		
+		std::string strin = "";
+		std::string strcp = "";
 		
 		int rand_primes [168] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 };
-		
-		std::string name = "";
-		long long int pin = 0;
-		
-		BigInteger encrypted_pin;
-		BigInteger decrypted_pin;
-		BigInteger dec_mult_pin;
 };
